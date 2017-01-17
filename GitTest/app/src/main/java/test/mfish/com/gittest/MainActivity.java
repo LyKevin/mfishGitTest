63ed0d97659d9ef50c9ad13b2a6fff1b512e742c
wjx <wjx@intoms.cn>
2017-01-17 17:39:12 +0800

    第一次提交

--- GitTest/app/src/main/java/test/mfish/com/gittest/MainActivity.java
+++ GitTest/app/src/main/java/test/mfish/com/gittest/MainActivity.java
@@ -1,16 +1,29 @@
 package test.mfish.com.gittest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int n = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("呵呵", "呵呵");
+        Person p = new Person();
+        p.setAge(18);
+        p.setName("ly");
+        for (int i = 0; i < 10; i++) {
+//            f1();
+            Log.i("i:--", i+"-");
+        }
+        Log.i(p.getName(), p.getAge()+"");
 
+    }
+    public void f1(){
+        n = 6;
     }
 }
